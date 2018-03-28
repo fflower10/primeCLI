47f21e0126fb09170ffd37abfa8e5edcd3c91581
unknown <francescofio90@gmail.com>
2018-03-28 19:36:12 +0200

    first commit

---
+++ Exercise.java
@@ -0,0 +1,30 @@
+package com.francesco.bean;
+
+public class Exercise {
+	
+	private String code;
+	private String description;
+	private MuscleBand muscleBand;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public MuscleBand getMuscleBand() {
+		return muscleBand;
+	}
+	public void setMuscleBand(MuscleBand muscleBand) {
+		this.muscleBand = muscleBand;
+	}
+	
+	
+
+}
